3688b9001a2d97a77392b204f9825c213b5f673e
Arlox <itsnotgamez11@gmail.com>
2025-04-27 17:08:44 +0930

    Update and rename id1.js to script.js

--- id1.js
+++
@@ -1,7 +0,0 @@
-function generateId() {
-const selectedType = document.getElementById('idType').value;
-let prefix = '';
-let randomString = '';
-if (selectedType === 'id1') {
-prefix = ''; // Prefix for 'Current' type ID
-randomString = Math.random().toString(36).substring(2, 15).toUpperCase()
